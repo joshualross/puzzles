26ecbe92585716374ce5d6efe35258d56dd98dd7
Joshua Ross <joshualross@gmail.com>
2013-09-12 10:22:21 -0700

    spiral output, solved

--- spiralOutput.php
+++ spiralOutput.php
@@ -16,19 +16,19 @@ How can you print them out in spiral order?  That is:
 function printTopRight($data, $x1, $y1, $x2, $y2)
 {
     //print the row
-    for ($i = $x1; $i < $x2; $i++)
+    for ($i = $x1; $i <= $x2; $i++)
     {
-        echo $data[$x1][$i] . ', ';
+        echo $data[$y1][$i] . ', ';
     }
 
-    //print the column
-    for ($j = $y1; $j < $y2; $j++)
+    //print the column -> start at next row
+    for ($j = $y1+1; $j <= $y2; $j++)
     {
-        echo $data[$y1][$j] . ', ';
+        echo $data[$j][$x2] . ', ';
     }
 
     if ($x2 - $x1 > 0)
-        printBottomLeft($data, $x2 - 1, $y2 - 1, $x1 + 1, $y1 + 1);
+        printBottomLeft($data, $x1, $y1+1, $x2-1, $y2);
 
 
 
@@ -37,19 +37,19 @@ function printTopRight($data, $x1, $y1, $x2, $y2)
 function printBottomLeft($data, $x1, $y1, $x2, $y2)
 {
     //print the row, right to left
-    for ($i = $x1; $i > $x2; $i--)
+    for ($i = $x2; $i >= $x1; $i--)
     {
-        echo $data[$x1][$i] . ', ';
+        echo $data[$y2][$i] . ', ';
     }
 
-    //print the column, bottom to top
-    for ($j = $y1; $j > $y2; $j--)
+    //print the column, start at next row, bottom to top
+    for ($j = $y2-1; $j >= $y1; $j--)
     {
-        echo $data[$y1][$i] . ', ';
+        echo $data[$j][$x1] . ', ';
     }
 
     if ($x2 - $x1 > 0)
-        printTopRight($data, $x1 + 1, $y1 + 1, $x2 - 1, $y2 - 1);
+        printTopRight($data, $x1 + 1, $y1, $x2, $y2 - 1);
 
 }
 
@@ -78,7 +78,7 @@ $tests = array(
 
 foreach ($tests as $test)
 {
-    printTopRight($test, 0, 0, count($test[0]), count($test));
+    printTopRight($test, 0, 0, count($test[0])-1, count($test)-1);
     echo PHP_EOL;
 
 }
